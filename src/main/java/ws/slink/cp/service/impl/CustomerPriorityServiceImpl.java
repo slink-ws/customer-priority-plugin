3d3b8e4c8c246c37be1d63d08bee07f9affbec88
Mikhail Kantur <kami@slink.ws>
2022-10-25 23:24:02 +1000

    admin section fix

--- src/main/java/ws/slink/cp/service/impl/CustomerPriorityServiceImpl.java
+++ src/main/java/ws/slink/cp/service/impl/CustomerPriorityServiceImpl.java
@@ -1,5 +1,6 @@
 package ws.slink.cp.service.impl;
 
+import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
@@ -17,11 +18,11 @@ import java.util.Optional;
 @JiraComponent
 public class CustomerPriorityServiceImpl implements CustomerPriorityService {
 
-    private ConfigService configService;
-    private JiraToolsService jiraToolsService;
-
+    private final ConfigService configService;
+    private final JiraToolsService jiraToolsService;
     @Inject
-    public CustomerPriorityServiceImpl(ConfigService configService, JiraToolsService jiraToolsService) {
+    public CustomerPriorityServiceImpl(ConfigService configService,
+                                       JiraToolsService jiraToolsService) {
         this.configService = configService;
         this.jiraToolsService = jiraToolsService;
     }
@@ -34,8 +35,11 @@ public class CustomerPriorityServiceImpl implements CustomerPriorityService {
     @Override
     public Optional<StyleElement> getStyleElement(String projectKey, String customerId) {
         for (StyleElement styleElement : configService.getStyles(projectKey)) {
-            if (jiraToolsService.templateMatch(styleElement.reporters(), customerId))
-                return Optional.of(styleElement);
+            if (jiraToolsService.templateMatch(styleElement.reporters(), customerId)) {
+                if (shouldColorize(projectKey, customerId)) {
+                    return Optional.of(styleElement);
+                }
+            }
         }
         return Optional.empty();
     }
@@ -54,6 +58,16 @@ public class CustomerPriorityServiceImpl implements CustomerPriorityService {
         if (styleElement.isPresent())
             return styleElement.get().text();
         return "";
+    }
+
+    private boolean shouldColorize(String projectKey, String customerId) {
+
+        String user = jiraToolsService.getLoggedInUser().getEmailAddress();
+        System.out.printf(
+            "~~~~~~~ project: %s, creator: %s, user: %s%n",
+            projectKey, customerId, user
+        );
+        return true;
     }
 
 }
